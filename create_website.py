81823180ea4898677588cb41c7e569d5772d1e0e
Uei Chen <ueichen0914@gmail.com>
2021-08-19 10:15:35 +0800

    update website

--- create_website.py
+++ create_website.py
@@ -3,9 +3,9 @@ from flask import Flask, redirect, url_for, render_template
 app = Flask(__name__)
 
 
-@app.route("/<name>")
-def home(name):
-    return render_template("index.html", content="Testing")
+@app.route("/")
+def home():
+    return render_template("upload.html", content="Testing")
 
 
 if __name__ == "__main__":
