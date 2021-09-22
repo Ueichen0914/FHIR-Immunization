73a614508e319d91421aad10a220c01f65ed0c33
UEI Chen <ueichen0914@gmail.com>
2021-09-22 18:08:18 +0800

    method fix

--- method/csv_to_json.py
+++ method/csv_to_json.py
@@ -3,29 +3,56 @@ import json
 import requests
 import logging
 
-header = {'Content-Type': 'application/json'}
-fhirbaseURL = 'https://hapi.fhir.tw/fhir/Patient'
+headers = {'Content-Type': 'application/json'}
 
 
-def patient(dic1):
+def patient(dic):
     with open(
-        "JSON_template\Patient_success_template.json", "r", encoding="utf-8"
-    ) as patient_json:
+            "JSON_template\Patient_template.json", "r", encoding="utf-8") as patient_json:
         patient = json.load(patient_json)
-    for key, value in dic1.items():
-        if key == "姓名":
-            patient["name"][0]["text"] = str(value)
-        elif key == "身分證":
-            patient["identifier"][0]["value"] = str(value)
-        elif key == "性別":
-            patient["gender"] = str(value)
-        elif key == "生日":
+    for key, value in dic.items():
+        if key == "LV_UUID":
+            # 加入id
+            patient["id"] = str(value)
+        elif key == "SEX":
+            # 判斷性別
+            if value == "1":
+                info = "male"
+            elif value == "2":
+                info = "female"
+            elif value == "3" | "4":
+                info = "other"
+            elif value == "9":
+                info = "unknown"
+            patient["gender"] = info
+        elif key == "BIRTH_Y":
+            # 判斷生日(民國)，放入svalue
             ivalue = int(value)
             if ivalue < 2000000:
                 ivalue += 19110000
             svalue = str(ivalue)
+            # 西元加入"-"放入ssvalue
             ssvalue = svalue[:4] + "-" + svalue[4:6] + "-" + svalue[6:8]
             patient["birthDate"] = ssvalue
+        elif key == "RESID":
+            # 加入地址
+            patient["address"][0]["postalCode"] = str(value)
+        # #deceased date出現問題先拿掉
+        # elif key == "FU_DT":
+        #     # 判斷生日(民國)，放入svalue
+        #     ivalue = int(value)
+        #     if ivalue < 2000000:
+        #         ivalue += 19110000
+        #     svalue = str(ivalue)
+        #     # 西元加入"-"放入ssvalue
+        #     ssvalue = svalue[:4] + "-" + svalue[4:6] + "-" + svalue[6:8]
+        #     patient["deceasedDateTime"] = ssvalue
+        elif key == "VSTATUS":
+            if value == "1":
+                info = False
+            else:
+                info = True
+            patient["deceasedBoolean"] = info
         else:
             pass
     return patient
@@ -36,15 +63,11 @@ def parse_csv(file):
     with open(file) as f:
         text = csv.DictReader(f)
         for line in text:
-            json_patient = json.dumps(
-                patient(line), sort_keys=False, indent=4, ensure_ascii=False
-            )
-            print(json_patient)
-            headers = {'Content-Type': 'application/json'}
-            rp = requests.post(
-                fhirbaseURL, headers=header, json=json_patient)
-            print("Status code : ", rp.status_code)
-            print("Print the entire Post Request : ")
-            print(rp.text)
-
-
+            json_patient = json.dumps(patient(line))
+            for key, value in line.items():
+                if key == "LV_UUID":
+                    fhirbaseURL = "https://hapi.fhir.tw/fhir/Patient/" + \
+                        str(value)
+            r_patient = requests.put(
+                fhirbaseURL, headers=headers, data=json_patient)
+            print(r_patient.text)
