42e901d2685e4da2bb9aecde1f6a723f3e25bb18
Ueichen0914 <71438453+Ueichen0914@users.noreply.github.com>
2021-10-21 14:29:16 +0800

    程式邏輯還是有點問題

--- bundle.py
+++ bundle.py
@@ -8,7 +8,42 @@ headers = {'Content-Type': 'application/json'}
 fhirbaseURL = "https://hapi.fhir.tw/fhir"
 
 
-def patient(dic, bundle):
+def patient(patient):
+    if key == "LV_UUID":
+        # 加入id
+        patient["id"] = str(value)
+    elif key == "SEX":
+        # 判斷性別
+        if value == "1":
+            info = "male"
+        elif value == "2":
+            info = "female"
+        elif value == "3" | "4":
+            info = "other"
+        elif value == "9":
+            info = "unknown"
+        patient["gender"] = info
+    elif key == "BIRTH_Y":
+        # 判斷生日(民國)，放入svalue
+        ivalue = int(value)
+        if ivalue < 2000000:
+            ivalue += 19110000
+        svalue = str(ivalue)
+        # 西元加入"-"放入ssvalue
+        ssvalue = svalue[:4] + "-" + svalue[4:6] + "-" + svalue[6:8]
+        patient["birthDate"] = ssvalue
+    elif key == "RESID":
+        # 加入地址
+        patient["address"][0]["postalCode"] = str(value)
+    elif key == "FU_DT":
+        # 判斷生日(民國)，放入svalue
+        ivalue = int(value)
+        if ivalue < 2000000:
+            ivalue += 19110000
+        svalue = str(ivalue)
+        # 西元加入"-"放入ssvalue
+        ssvalue = svalue[:4] + "-" + svalue[4:6] + "-" + svalue[6:8]
+        patient["deceasedDateTime"] = ssvalue
     return patient
 
 
@@ -157,6 +192,11 @@ with open(file) as f:
     text = csv.DictReader(f)
     for line in text:
         with open(
-                "JSON_template\Bundle.json", "r", encoding="utf-8") as Bundle_json:
+                "Bundle_template\Bundle.json", "r", encoding="utf-8") as Bundle_json:
             bundle = json.load(Bundle_json)
-        patient(line, bundle)
+        with open(
+            "Bundle_template\Bundle_Patient.json", "r", encoding="utf-8") as patient_json:
+            patient = json.load(patient_json)
+        for key,value in line.items():
+            patient(patient)
+        for key,value in line.items():
