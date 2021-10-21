438f14a2a3b62f83ace01fb86d9fdb07b09e316c
UEI <ueichen0914@gmail.com>
2021-10-21 21:56:57 +0800

    update

--- bundle.py
+++ bundle.py
@@ -1,49 +1,58 @@
 import csv
 import json
 import requests
-import datetime
+from datetime import datetime
 import time
 
 headers = {'Content-Type': 'application/json'}
 fhirbaseURL = "https://hapi.fhir.tw/fhir"
 
 
-def patient(patient):
-    if key == "LV_UUID":
-        # 加入id
-        patient["id"] = str(value)
-    elif key == "SEX":
-        # 判斷性別
-        if value == "1":
-            info = "male"
-        elif value == "2":
-            info = "female"
-        elif value == "3" | "4":
-            info = "other"
-        elif value == "9":
-            info = "unknown"
-        patient["gender"] = info
-    elif key == "BIRTH_Y":
-        # 判斷生日(民國)，放入svalue
-        ivalue = int(value)
-        if ivalue < 2000000:
-            ivalue += 19110000
-        svalue = str(ivalue)
-        # 西元加入"-"放入ssvalue
-        ssvalue = svalue[:4] + "-" + svalue[4:6] + "-" + svalue[6:8]
-        patient["birthDate"] = ssvalue
-    elif key == "RESID":
-        # 加入地址
-        patient["address"][0]["postalCode"] = str(value)
-    elif key == "FU_DT":
-        # 判斷生日(民國)，放入svalue
-        ivalue = int(value)
-        if ivalue < 2000000:
-            ivalue += 19110000
-        svalue = str(ivalue)
-        # 西元加入"-"放入ssvalue
-        ssvalue = svalue[:4] + "-" + svalue[4:6] + "-" + svalue[6:8]
-        patient["deceasedDateTime"] = ssvalue
+def patient(dic):
+    with open("Bundle_template\Bundle_Patient.json", "r", encoding="utf-8") as patient_json:
+        patient_r = json.load(patient_json)
+        patient = patient_r["resource"]
+    for key, value in dic.items():
+        if key == "LV_UUID":
+            # 加入id
+            patient["id"] = str(value)
+            patient_r["request"]["url"] = fhirbaseURL + "/Patient/" + str(value)
+        elif key == "SEX":
+            # 判斷性別
+            if value == "1":
+                info = "male"
+            elif value == "2":
+                info = "female"
+            elif value == "3" | "4":
+                info = "other"
+            elif value == "9":
+                info = "unknown"
+            patient["gender"] = info
+        elif key == "BIRTH_Y":
+            # 判斷生日(民國)，放入svalue
+            ivalue = int(value)
+            if ivalue < 2000000:
+                ivalue += 19110000
+            svalue = str(ivalue)
+            # 西元加入"-"放入ssvalue
+            ssvalue = svalue[:4] + "-" + svalue[4:6] + "-" + svalue[6:8]
+            try:
+                d_format = datetime.strptime(svalue, "%Y-%m-%d")
+            except:
+                continue
+            patient["birthDate"] = d_format.strftime("%Y-%m-%d")
+        elif key == "RESID":
+            # 加入地址
+            patient["address"][0]["postalCode"] = str(value)
+        elif key == "FU_DT":
+            # 判斷生日(民國)，放入svalue
+            ivalue = int(value)
+            if ivalue < 2000000:
+                ivalue += 19110000
+            svalue = str(ivalue)
+            # 西元加入"-"放入ssvalue
+            ssvalue = svalue[:4] + "-" + svalue[4:6] + "-" + svalue[6:8]
+            patient["deceasedDateTime"] = ssvalue
     return patient
 
 
@@ -194,9 +203,6 @@ with open(file) as f:
         with open(
                 "Bundle_template\Bundle.json", "r", encoding="utf-8") as Bundle_json:
             bundle = json.load(Bundle_json)
-        with open(
-            "Bundle_template\Bundle_Patient.json", "r", encoding="utf-8") as patient_json:
-            patient = json.load(patient_json)
         for key,value in line.items():
-            patient(patient)
-        for key,value in line.items():
+            patient(line)
+            
