80a93d34dd64ab7f29de3698555f3dff3b8364f6
Ueichen0914 <71438453+Ueichen0914@users.noreply.github.com>
2021-10-26 23:43:39 +0800

    last change

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
-from datetime import datetime
+from datetime import date
 
-value = "20200505"
-svalue = value[:4] + "-" + value[4:6] + "-" + value[6:8]
-print(svalue)
-try:
-    d_format = datetime.strptime(svalue, "%Y-%m-%d")
-except:
-    print("hello world")
-print(d_format.strftime("%Y-%m-%d"))
+# value = "20200505"
+# svalue = value[:4] + "-" + value[4:6] + "-" + value[6:8]
+# print(svalue)
+# try:
+#     d_format = datetime.strptime(svalue, "%Y-%m-%d")
+# except:
+#     print("hello world")
+# print(d_format.strftime("%Y-%m-%d"))
+print(date.today())
